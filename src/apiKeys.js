aa947c30018de43d6ff75ab776a8ad858bd813c9
Pankaj Verma <pankajverma@Pankajs-MacBook-Pro.local>
2024-01-13 11:47:03 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "cabbf40abd3ce4a222a1608f6351615f",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
